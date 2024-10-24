43fb14747700b78f047da160bd55585ca4dd7810
Muhammad Lukmanhakim Bin Ahmad <lukkim46@gmail.com>
2024-10-24 11:35:06 +0800

    Add project files.

---
+++ NumberGuessGame/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace NumberGuessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random random = new Random();
+
+            int randomNum = random.Next(1, 11);
+
+            Console.WriteLine("Welcome to number guessing game ...");
+            Console.WriteLine("A number between 1 to 10 will be generated.");
+            Console.WriteLine("if you guess the correct number, you win");
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("enter your guess");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess > randomNum)
+                {
+                    Console.WriteLine("your gess to high");
+                }
+                else if(guess < randomNum)
+                {
+                    Console.WriteLine("your guess to low");
+                }
+                else
+                {
+                    Console.WriteLine("Correct");
+                    isCorrectGuess = true;
+                }
+            }
+
+            Console.WriteLine("You won the game");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
